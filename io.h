351cd1a3261127926b059dc4028f62db98f9c473
JasonQSY <syqian@sjtu.edu.cn>
2016-12-21 16:06:14 +0800

    add docs and implement all 16 colors

--- io.h
+++ io.h
@@ -1,6 +1,14 @@
 #ifndef INCLUDE_IO_H
 #define INCLUDE_IO_H
 
-void outb(unsigned short port, unsigned char bar);
+/**
+ * Send data to the port (I/O ports generally).
+ *
+ * Implement in io.s
+ *
+ * @param port the I/O port
+ * @param data the data
+ */
+void outb(unsigned short port, unsigned char data);
 
 #endif /* INCLUDE_IO_H */
--- kmain.c
+++ kmain.c
@@ -1,12 +1,46 @@
 #include <io.h>
 
+/**
+ * For the framebuffer,
+ *
+ * example 0x4128
+ * 41: ascii of character
+ * 2: foreground color
+ * 8: background color
+ * hence it is defined as follows.
+ *
+ * @TODO split them into separate header file.
+ */
+
 /* colors */
+#define FB_BLACK 0
+#define FB_BLUE 1
 #define FB_GREEN 2
+#define FB_CYAN 3
+#define FB_RED 4
+#define FB_MAGENTA 5
+#define FB_BROWN 6
+#define FB_LIGHT_GREY 7
 #define FB_DARK_GREY 8
+#define FB_LIGHT_BLUE 9
+#define FB_LIGHT_GREEN 10
+#define FB_LIGHT_CYAN 11
+#define FB_LIGHT_RED 12
+#define FB_LIGHT_MAGENTA 13
+#define FB_LIGHT_BROWN 14
+#define FB_WHITE 15
 
 /* framebuffer mm io */
 char *fb = (char *) 0x000B8000;
 
+/**
+ * display character c on the position i with color fg and bg.
+ *
+ * @param i the position, 0 for the first line, first col. 16 for the second line.
+ * @param c the displayed character
+ * @param fg foreground color
+ * @param bg background color
+ */
 void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg) {
     fb[i] = c;
     fb[i + 1] = ((fg & 0x0F) << 4 | (bg & 0x0F));
@@ -19,6 +53,13 @@ void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg) {
 #define FB_HIGH_BYTE_COMMAND 14
 #define FB_LOW_BYTE_COMMAND 15
 
+/**
+ * move the cursor to the given position
+ *
+ * @param short pos: 16 bit totally
+ *      - first 8 bit: the row
+ *      - last 8 bit: the colomn
+ */
 void fb_move_cursor(unsigned short pos) {
     outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
     outb(FB_DATA_PORT, ((pos >> 8) & 0x00FF));
@@ -27,7 +68,11 @@ void fb_move_cursor(unsigned short pos) {
 }
 
 int main() {
+    /* display a 'A' in the first line, first column */
     fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);
+
+    /* display the cursor in the second line */
     fb_move_cursor(0x0050);
+
     return 0;
 }
