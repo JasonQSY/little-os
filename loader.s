bf0809ff4d6e4a5dbcb0388543b60c358da3982c
JasonQSY <syqian@sjtu.edu.cn>
2016-12-22 21:45:52 +0800

    fix typo of kernel

--- loader.s
+++ loader.s
@@ -3,14 +3,14 @@ global loader
 MAGIC_NUMBER equ 0x1BADB002
 FLAGS equ 0x0
 CHECKSUM equ -MAGIC_NUMBER
-KERNAL_STACK_SIZE equ 4096
+KERNEL_STACK_SIZE equ 4096
 
 extern main
 
 section .bss
 align 4
-kernal_stack:
-    resb KERNAL_STACK_SIZE
+kernel_stack:
+    resb KERNEL_STACK_SIZE
 
 section .text
 align 4
@@ -19,7 +19,7 @@ align 4
     dd CHECKSUM
 
 loader:
-    mov esp, kernal_stack + KERNAL_STACK_SIZE
+    mov esp, kernel_stack + KERNEL_STACK_SIZE
     call main
     mov eax, 0xCAFEBABE
 .loop:
